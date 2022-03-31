d69007a4ee8d8b0a26ca834690a07f7637c55263
Priyanka Patel <priyanka.patel@ibm.com>
2022-03-31 18:12:48 -0400

    adding swagger code

--- user-api/src/main/java/com/ibm/api/userapi/UserApiApplication.java
+++ user-api/src/main/java/com/ibm/api/userapi/UserApiApplication.java
@@ -2,10 +2,12 @@ package com.ibm.api.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "User Registrations API", version = "1.0", description = "User Registration Information"))
 public class UserApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserApiApplication.class, args);
 	}
